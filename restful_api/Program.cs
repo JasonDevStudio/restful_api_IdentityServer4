092e4228e2c7f4dd5149ed43db2f18b82fad6fdf
jiedevstudio <jiedev@163.com>
2018-11-24 23:40:46 +0800

    added

--- restful_api/Program.cs
+++ restful_api/Program.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace restful_api
 {
@@ -19,7 +12,7 @@ namespace restful_api
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-            .UseUrls("http://localhost:57396")
+            .UseUrls("http://localhost:8081")
                 .UseStartup<Startup>();
     }
 }
---
+++ restful_client/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using RestSharp;
+
+namespace restful_client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            try
+            {
+                var client = new RestClient(@"http://localhost:8081");
+                var request = new RestRequest("connect/token", Method.POST,DataFormat.Json);
+                request.AddParameter("grant_type", "client_credentials", ParameterType.RequestBody);
+                request.AddParameter("client_id", "app_lds", ParameterType.RequestBody);
+                request.AddParameter("client_secret", "jgktjgkt99", ParameterType.RequestBody);
+
+                var resp = client.Execute(request);
+                Console.WriteLine(resp.StatusCode);
+                Console.WriteLine(resp.Content);
+
+                request = new RestRequest("api/default",Method.GET);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                var key = string.Empty;
+
+                do
+                {
+                    key = Console.ReadLine();
+                } while (key.ToUpper() != "EXIT");
+            }
+        }
+    }
+}
